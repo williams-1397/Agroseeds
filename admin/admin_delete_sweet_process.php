736e27679ac470b632cff3c3d89b0c04708759e5
williams-1397 <50476105+williams-1397@users.noreply.github.com>
2021-09-16 18:12:58 +0530

    Page to delete product
    page to delete product.

---
+++ admin/admin_delete_sweet_process.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'database.php';
+error_reporting(E_ERROR | E_PARSE);
+session_start();
+if(isset($_SESSION['admin_login_user']))
+{
+  $sql = "DELETE FROM product WHERE id='" . $_GET["id"] . "'";
+  if (mysqli_query($conn, $sql))
+  {
+    echo "<script>alert('record deleted suceessfully!')
+		window.location.href='admin_show_available_product.php';</script>";
+  }
+  else
+  {
+    echo "<script>alert('record not deleted.!')
+		window.location.href='admin_show_available_product.php';</script>";
+  }
+  mysqli_close($conn);
+}
+else
+{
+  echo "<script>alert('PLEASE LOGIN FIRST')
+	window.location.href='index.php';</script>";
+}
+?>
